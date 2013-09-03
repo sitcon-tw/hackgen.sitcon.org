b78a5df76090f400636a5390d5556074dc15d41f
蒼時弦也 <elct9620@frost.tw>
2013-09-03 13:43:18 +0800

    Update# Setting up build config

--- config.rb
+++ config.rb
@@ -23,17 +23,18 @@ javascripts_dir = "javascripts"
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
 
-css_dir = "stylesheets" # by Fire.app 
-sass_dir = "sass" # by Fire.app 
-images_dir = "images" # by Fire.app 
-javascripts_dir = "javascripts" # by Fire.app 
-fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
-fireapp_livescripts_dir = "livescripts" # by Fire.app 
-fireapp_minifyjs_on_build = true # by Fire.app 
-fireapp_always_report_on_build = true # by Fire.app 
-output_style = :compressed # by Fire.app 
-relative_assets = true # by Fire.app 
-line_comments = false # by Fire.app 
-sass_options = {:debug_info=>false} # by Fire.app 
-fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
-fireapp_livescript_options = {:bare=>false} # by Fire.app 
+css_dir = "stylesheets" # by Fire.app
+sass_dir = "sass" # by Fire.app
+images_dir = "images" # by Fire.app
+javascripts_dir = "javascripts" # by Fire.app
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app
+fireapp_livescripts_dir = "livescripts" # by Fire.app
+fireapp_minifyjs_on_build = true # by Fire.app
+fireapp_always_report_on_build = false # by Fire.app
+output_style = :compressed # by Fire.app
+relative_assets = true # by Fire.app
+line_comments = false # by Fire.app
+sass_options = {:debug_info=>false} # by Fire.app
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app
+fireapp_livescript_options = {:bare=>false} # by Fire.app
+fireapp_build_path="build"
